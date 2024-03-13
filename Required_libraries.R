f65267f007dbec93552dc37113123918f6cab9e2
Loïc Talide <loic.talide@slu.se>
2024-03-13 16:37:15 +0100

    Start of github implementation

---
+++ Required_libraries.R
@@ -0,0 +1,30 @@
+# Verify and install necessary libraries
+if (!require("plotly")) {
+  install.packages("plotly")
+  library("plotly")
+}
+
+if (!require("flexdashboard")) {
+  install.packages("flexdashboard")
+  library("flexdashboard")
+}
+
+if (!require("dplyr")) {
+  install.packages("dplyr")
+  library("dplyr")
+}
+
+if (!require("shiny")) {
+  install.packages("shiny")
+  library("shiny")
+}
+
+if (!require("ggplot2")) {
+  install.packages("ggplot2")
+  library("ggplot2")
+}
+
+if (!require("plyr")) {
+  install.packages("plyr")
+  library("plyr")
+}
